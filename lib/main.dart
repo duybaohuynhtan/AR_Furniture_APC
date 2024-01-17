db7952dbdcb8354834385795c8179e6ecf5ca8a2
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-01-17 11:33:29 +0900

    login function (init)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:ar_furniture_app/onboarding_page.dart';
 import 'package:csv/csv.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,6 +12,10 @@ void main() async {
     "assets/product_data.csv",
   );
   List<List<dynamic>> productList = const CsvToListConverter().convert(csvData);
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     MaterialApp(
